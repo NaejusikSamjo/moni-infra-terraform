635e91f4ded7fbcf8e87aa9169f070bfaf0dbc36
won2dev-backand <seodongwoncp@gmail.com>
2026-07-02 13:53:10 +0900

    feat: CDN 이미지 CORS 허용 추가

--- s3/main.tf
+++ s3/main.tf
@@ -2,6 +2,17 @@ resource "aws_s3_bucket" "this" {
   bucket = var.bucket_name
 }
 
+resource "aws_s3_bucket_cors_configuration" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  cors_rule {
+    allowed_headers = ["content-type"]
+    allowed_methods = ["PUT"]
+    allowed_origins = ["https://moni.my"]
+    max_age_seconds = 3000
+  }
+}
+
 resource "aws_s3_bucket_public_access_block" "this" {
   bucket = aws_s3_bucket.this.id
 
